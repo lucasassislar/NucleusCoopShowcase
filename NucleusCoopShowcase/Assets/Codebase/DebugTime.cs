371937a17a2aa6ccad1c8e4b59a4b708112d2040
Lucas Assis <lucasassis@outlook.com>
2020-02-27 04:07:11 -0300

    timeline updates

---
+++ NucleusCoopShowcase/Assets/Codebase/DebugTime.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace NucleusShowcase {
+    public class DebugTime : MonoBehaviour {
+        private GUIStyle guiStyle;
+
+        public void OnGUI() {
+            if (guiStyle == null) {
+                guiStyle = new GUIStyle();
+                guiStyle.fontSize = 32;
+            }
+
+            GUI.Label(new Rect(20, 20, 100, 100), Time.time.ToString(), guiStyle);
+        }
+    }
+}
